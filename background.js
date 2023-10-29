9decd47e67565bd3da8f3a8ffdda9dc3d01eccc1
lokesh.bandi <lokesh.bandi@pragmaticplay.com>
2023-10-29 14:10:52 +0530

    Displaying meaning in chrome itself

--- background.js
+++ background.js
@@ -1,4 +1,15 @@
-chrome.runtime.onMessage.addListener((message) => {
-  console.log(message)
-  chrome.storage.sync.set({'selectedText': message.selectedText});
+chrome.runtime.onMessage.addListener(async (message) => {
+  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${message.selectedText}`;
+  let fetchedData;
+  try {
+      const response = await fetch(url);
+      fetchedData = await response.json();
+      console.log(fetchedData);
+  } catch (error) {
+      console.error(error);
+  }
+  chrome.storage.sync.set({
+    'selectedText': message.selectedText,
+    'wordInfo': fetchedData
+  });
 });
--- controller.js
+++ controller.js
@@ -1,24 +1,32 @@
-document.addEventListener("selectionchange", () => {
-    if(document.getSelection().toString()){
-        document.getElementById('searchWord').value = document.getSelection().toString();
-    }
-});
+
+// const searchBtn = document.getElementsByClassName('submitBtn')[0];
+// searchBtn.addEventListener('click', () => {
+//     let searchContent = document.getElementById('searchWord').value;
+//     // let newURL = `https://www.google.com/search?q=${searchContent}+meaning+in+english`;
+//     // chrome.tabs.create({ url: newURL });
+
+// })
 
 
-const searchBtn = document.getElementsByClassName('submitBtn')[0];
-searchBtn.addEventListener('click', () => {
-    console.log(window.getSelection().toString(), chrome)
-    let searchContent = document.getElementById('searchWord').value;
-    let newURL = `https://www.google.com/search?q=${searchContent}+meaning+in+english`;
-    chrome.tabs.create({ url: newURL });
-})
+const processData = async (fetchedData) => {
+  const resultDiv = document.getElementsByClassName('result')[0];
+  fetchedData[0].meanings.forEach(eachMeaning => {
+      eachMeaning.definitions.forEach((eachDefinition) => {
+        const childElement = document.createElement('div');
+        childElement.classList.add('contentBox')
+        childElement.appendChild(document.createTextNode(eachDefinition.definition));
+        resultDiv.appendChild(childElement)
+    })
+  })
 
-
+}
 function updateText(){
     chrome.storage.sync.get(['selectedText'], function(data) {
       document.getElementById("searchWord").value = data.selectedText;
-  
+    });
+    chrome.storage.sync.get(['wordInfo'], function(data) {
+        processData(data.wordInfo)
     });
   }
   
-  document.addEventListener('DOMContentLoaded', updateText);
+document.addEventListener('DOMContentLoaded', updateText);
